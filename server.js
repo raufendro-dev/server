7486a30cf17124ce70dda132d0f1520bdae52799
raufendro-dev <raufendro@gmail.com>
2024-08-23 14:40:13 +0700

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 80;
+const port = 8080;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
